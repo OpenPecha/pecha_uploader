a878931f79adfd30797e682f7b561bd326cfa8a8
lobsam <sanduplobzang@gmail.com>
2025-03-20 11:03:46 +0530

    rollback all category if there is no other content in category

--- src/pecha_uploader/clear_unfinished_text.py
+++ src/pecha_uploader/clear_unfinished_text.py
@@ -9,6 +9,15 @@ def remove_texts_meta(meta_list: dict, destination_url: Destination_url):
     if "term" in meta_list:
         remove_term(meta_list["term"], destination_url)
     if "category" in meta_list:
-        remove_category(meta_list["category"], destination_url)
+        for cat in transform_category_list(meta_list["category"]):
+            remove_category(cat, destination_url)
     if "index" in meta_list:
         remove_index(meta_list["index"], destination_url)
+
+
+def transform_category_list(categories):
+    """
+    Transform a flat list of categories into nested sublists in reverse order.
+    Example: ["a", "b", "c"] -> [["a", "b", "c"], ["a", "b"], ["a"]]
+    """
+    return [categories[: i + 1] for i in range(len(categories) - 1, -1, -1)]
